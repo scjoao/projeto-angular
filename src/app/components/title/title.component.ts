37b3fab20ba478165790fab063ac1621195711c3
João Carlos <joao.carlos_25@hotmail.com>
2023-09-03 14:01:05 -0300

    ngOnChanges

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,27 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-title',
 	templateUrl: './title.component.html',
 	styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
-	nome: string = "João";
+	@Input()
+	nome: string = "";
 
 	constructor() {
-
+		console.log(`Olá, eu sou o construtor: ${this.nome}`);
 	}
 
 	ngOnInit(): void {
-		//Printa o nome quando o componente nasce.
-		console.log(this.nome);
+		console.log(`Olá, eu sou o ngOnInit: ${this.nome}`);
+	}
 
-		//Logo depois concatena o nome.
-		this.nome = `Olá ${this.nome}`;
-
-		//Printa o nome concatenado.
-		console.log(this.nome);
+	//Atrelado a qualquer mudança e atribuição de valor que acontece via @Input
+	ngOnChanges(): void {
+		console.log(`Olá, eu sou o ngOnChanges: ${this.nome}`);
 	}
 
 }
